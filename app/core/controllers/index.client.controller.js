14e5771eaf9e93b18dafb3a3de6493e60d1eb270
Eric Demauro <erickdemauro@gmail.com>
2016-05-26 19:19:55 -0700

    Ported DB deletion modal to angular.

--- app/core/controllers/index.client.controller.js
+++ app/core/controllers/index.client.controller.js
@@ -3,10 +3,11 @@
 
   angular
     .module('app')
-    .controller('IndexController', IndexController);
+    .controller('IndexController', IndexController)
+    .controller('ModalController', ModalController);
 
-  function IndexController(BreadCrumbsService) {
-    var vm = this;
+  function IndexController(BreadCrumbsService, $uibModal) {
+    let vm = this;
     vm.breadcrumbs = BreadCrumbsService.breadcrumbs;
     BreadCrumbsService.getIndex()
       .then(() => {
@@ -15,7 +16,36 @@
 
     vm.deleteDb = (db) => {
       // still need to implement deletion + whatever normally happens after deleting
-      console.log(db);
+      let modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'deleteModalContent.html',
+        controller: 'ModalController',
+        controllerAs: 'vm',
+        resolve: {
+          db: () => {
+            return db;
+          }
+        }
+      });
+
+      modalInstance.result.then((selectedItem) => {
+        console.log(selectedItem);
+      }, () => {
+        console.log('Modal dismissed at: ' + new Date());
+      });
     };
   };
+
+  function ModalController($uibModalInstance, db) {
+    let vm = this;
+
+    vm.database = db;
+    vm.dbConfirm = "";
+
+    // send delete http request IF dbconfirm === db
+
+    vm.close = () => {
+      $uibModalInstance.close();
+    };
+  }
 })();
