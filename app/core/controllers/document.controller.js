75451158f996310f86713428ea34425b9e2f027e
Eric Demauro <erickdemauro@gmail.com>
2016-06-10 15:33:51 -0700

    Added angular code mirror to document view along with options.

--- app/core/controllers/document.controller.js
+++ app/core/controllers/document.controller.js
@@ -9,6 +9,15 @@
     let vm = this;
     
     vm.context = ContextService.context;
+    vm.doc = vm.context.ctx.docString;
+    vm.editorOptions = { 
+      mode: { name: 'javascript', json: true },
+      indentUnit: 4,
+      lineNumbers: true,
+      autoClearEmptyLines: true,
+      matchBrackets: true,
+      readOnly: vm.context.readOnly
+    };
 
     console.log(vm.context);
   }
