55200be066ed4f81dcdd0dfa7f8f8a4520ebc1f5
Eric Demauro <erickdemauro@gmail.com>
2016-06-13 19:45:49 -0700

    added alerts for gridfs controller. fixed bugs in collection view (pagination wasn't hidden w/ 0 docs, 1 NaN error, "do docs found" wasn't displaying, and displaying index values)

--- app/core/controllers/gridfs.controller.js
+++ app/core/controllers/gridfs.controller.js
@@ -15,25 +15,44 @@
     ContextService.setActiveTemplate(2);
 
     function addFile() {
+      if(!vm.file) {
+        ContextService.addAlert({type: 'danger', msg: 'No file chosen.' });
+        return;
+      }
+
       return GridfsService.addFile($stateParams.database, $stateParams.bucket, vm.file)
-        .then((response) => {
-          console.log(response);
-          $state.go('database',{
-            'database': $stateParams.database
-          });
+        .then(addFileCompleted)
+        .catch(addFileFailed);
+
+      function addFileCompleted(response) {
+        ContextService.addAlert({type: 'success', msg: response.message });
+
+        $state.go('database',{
+          'database': $stateParams.database
         });
+      }
+
+      function addFileFailed(response) {
+        ContextService.addAlert({type: 'danger', msg: response.message });
+      }
     }
 
     function deleteFile(file) {
       return GridfsService.deleteFile($stateParams.database, $stateParams.bucket, file)
-        .then((response) => {
-          console.log(response);
-          $state.go('database',{
-            'database': $stateParams.database
-          });
-        })
-    }
+        .then(deleteFileCompleted)
+        .catch(deleteFileFailed);
 
-    console.log(vm.context);
+      function deleteFileCompleted(response) {
+        ContextService.addAlert({type: 'success', msg: response.message });
+
+        $state.go('database',{
+          'database': $stateParams.database
+        });
+      }
+
+      function deleteFileFailed(response) {
+        ContextService.addAlert({type: 'danger', msg: response.message });
+      }
+    }
   };
 })();
