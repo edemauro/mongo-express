7994a0993256ca3c5f646bc48d68fc9aafffe11f
Eric Demauro <erickdemauro@gmail.com>
2016-06-16 21:39:36 -0700

    database service spec.

---
+++ tests/services/database.service.spec.js
@@ -0,0 +1,35 @@
+describe('ContextService', function() {
+  beforeEach(module('app'));
+
+  var $httpBackend,
+    DatabaseService;
+
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    DatabaseService = $injector.get('DatabaseService');
+    $httpBackend.whenGET('/angular/core/views/index.view.html').respond(200, '');
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should add a database', function() {
+    $httpBackend.expectPOST('/', {database: 'database'})
+      .respond(200, { message: 'database added'});
+
+    DatabaseService.addDatabase('database');
+
+    expect($httpBackend.flush).not.toThrow();
+  });
+
+  it('should delete a database', function() {
+    $httpBackend.expectDELETE('/database')
+      .respond(200, { message: 'database deleted'});
+
+    DatabaseService.deleteDatabase('database');
+
+    expect($httpBackend.flush).not.toThrow();
+  });
+});
