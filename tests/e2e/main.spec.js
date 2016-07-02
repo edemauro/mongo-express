ff210e1d32bb4de5ea1922cf6f82f00acef34880
Eric Demauro <erickdemauro@gmail.com>
2016-07-02 00:05:28 -0700

    es6 refactoring. Added e2e tests for collection view.

--- tests/e2e/main.spec.js
+++ tests/e2e/main.spec.js
@@ -1,40 +1,66 @@
-describe('E2E:', function() {
-  it('should see the homepage', function() {
-    browser.get('http://admin:pass@localhost:8081');
-
-    element(by.id('pageTitle')).getText().then(function(text) {
-        expect(text).toEqual('Mongo Express');
-    });
-  });
-
-  describe('Home view:', function() {
-    beforeEach(function() {
+describe('E2E:', () => {
+  describe('Home view:', () => {
+    beforeEach(() => {
       browser.get('http://admin:pass@localhost:8081');
     });
 
-    it('should be able to create a database', function() {
+    it('should see the homepage', () => {
+      element(by.id('pageTitle')).getText().then(text => {
+        expect(text).toEqual('Mongo Express');
+      });
+    });
+
+    it('should be able to create a database', () => {
       element(by.model('vm.database')).sendKeys('a_database');
       element(by.css('.pull-right')).click();
 
-      element(by.css('.alert-success span.ng-scope')).getText().then(function (text) {
+      element(by.css('.alert-success span.ng-scope')).getText().then(text => {
         expect(text).toEqual('database created.');
       });
     });
 
-    it('should be able to delete a database', function() {
+    it('should be able to delete a database', () => {
       element.all(by.css('[ng-click="vm.deleteDb(db)"]')).get(0).click();
 
-      element(by.css('#confirmationInput')).isDisplayed().then(function(result) {
+      element(by.css('#confirmationInput')).isDisplayed().then(result => {
         if(result) {
           element(by.css('#confirmationInput')).sendKeys('a_database');
           element(by.css('[ng-click="vm.deleteDb()"]')).click();
         }
       });
 
-      browser.sleep(6000);
-    
-      element(by.css('.alert-success span.ng-scope')).getText().then(function (text) {
+      element(by.css('.alert-success span.ng-scope')).getText().then(text => {
         expect(text).toEqual('database dropped.');
+      });
+    });
+  });
+
+  describe('database view:', () => {
+    beforeEach(() => {
+      browser.get('http://admin:pass@localhost:8081/#/db/local');
+    });
+
+    it('should be able to create a collection', () => {
+      element(by.model('vm.collection')).sendKeys('a_collection');
+      element(by.css('.pull-right')).click();
+
+      element(by.css('.alert-success span.ng-scope')).getText().then(text => {
+        expect(text).toEqual('Collection created!');
+      });
+    });
+
+    it('should be able to delete a collection', () => {
+      element.all(by.css('[ng-click="vm.deleteCollection(c)"]')).get(0).click();
+
+      element(by.css('#confirmationInput')).isDisplayed().then(result => {
+        if(result) {
+          element(by.css('#confirmationInput')).sendKeys('a_collection');
+          element(by.css('[ng-click="vm.deleteCollection()"]')).click();
+        }
+      });
+
+      element(by.css('.alert-success span.ng-scope')).getText().then(text => {
+        expect(text).toEqual('Collection "a_collection" deleted!');
       });
     });
   });
